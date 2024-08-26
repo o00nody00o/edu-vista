850cec4820a975ab5b749d3944959d8befbc6173
o00nody00o <s.p002@outlook.com>
2024-08-26 22:48:15 +0300

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,58 @@
+import 'package:edu_vista/models/category.dart';
+import 'package:edu_vista/widgets/categories_widget.dart';
+import 'package:edu_vista/widgets/courses_widget.dart';
+import 'package:edu_vista/widgets/label_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  static const String id = 'home';
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            'Welcome Back! ${FirebaseAuth.instance.currentUser?.displayName}'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              LabelWidget(
+                name: 'Categories',
+                onSeeAllClicked: () {},
+              ),
+              CategoriesWidget(),
+              const SizedBox(
+                height: 20,
+              ),
+              LabelWidget(
+                name: 'Top Rated Courses',
+                onSeeAllClicked: () {},
+              ),
+              const CoursesWidget(
+                rankValue: 'top rated',
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    await FirebaseFirestore.instance
+                        .collection('test')
+                        .doc('x')
+                        .delete();
+                  },
+                  child: Text('test'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
